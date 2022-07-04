9270afe7ecb4377867abb7b54fce229ea2fa32b8
Fotis Gogos <83247314+FotisGogos@users.noreply.github.com>
2022-07-04 17:48:01 +0200

    4.3 styleUrls was removed due to a bug it was causing

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -14,7 +14,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+  
 })
 export class UserRegistrationFormComponent implements OnInit {
 
